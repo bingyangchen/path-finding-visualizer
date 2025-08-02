5d06b54258f05155dd2b2b5545f81c0ea62897a4
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-08-02 14:24:12 +0800

    Refactor deploy script to improve asset backup and restore process, ensuring a smoother deployment to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,25 @@
 #!/usr/bin/env bash
 
+git switch main
 npm run build
-git stash push -m "built artifact" -- assets/ js/ index.html style.css
+
+cp -r js/ /tmp/main_js_backup
+cp style.css /tmp/main_style_backup.css
+
 git switch gh-pages
-git rm -rf --ignore-unmatch assets/ js/ index.html style.css
-git stash pop
+git show main:index.html > index.html
+
+rm -rf assets/
+git checkout main -- assets/
+
+rm -rf js/
+cp -r /tmp/main_js_backup js/
+
+cp /tmp/main_style_backup.css style.css
+
+rm -rf /tmp/main_js_backup
+rm /tmp/main_style_backup.css
+
 git add -A
 git commit -m "deploy"
 git push origin gh-pages
