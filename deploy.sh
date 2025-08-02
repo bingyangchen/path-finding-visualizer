e65978d40b3b895daf9292c20e3631418bd0992d
Bing-Yang Chen <bryan.chen.429@gmail.com>
2025-08-02 14:05:33 +0800

    Update deploy script to build artifacts before deployment and streamline asset management

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+npm run build
+git stash push -m "built artifact" -- assets/ js/ index.html style.css
 git switch gh-pages
 git rm -rf --ignore-unmatch assets/ js/ index.html style.css
-git restore --source=main --staged --worktree -- assets/ js/ index.html style.css
+git stash pop
 git add -A
 git commit -m "deploy"
 git push origin gh-pages
